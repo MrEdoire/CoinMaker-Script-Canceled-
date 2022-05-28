efd1879d504e5605880d71e08bbb5c1a83bcacf5
MrEdoire <macarezedouard@gmail.com>
2022-05-28 22:18:13 +0200

    Making the code cleaner
    Dividing the code in one file to get the data, one to apply the strategy, and one another for the analysis.

---
+++ backtest/get_data.py
@@ -0,0 +1,50 @@
+from settings import *
+from api import *
+import pandas as pd
+
+class GetData:
+
+    def __init__(self):
+
+        self.client = Client(api_key, api_secret)
+
+        self.fees = float(self.client.get_trade_fee(symbol=Settings.currency)[0]['takerCommission'])
+
+        # Used for analysis
+        self.start_wallet = Settings.usdt
+
+        self.get_historical_data()
+
+    def get_historical_data(self):
+        # Get a dataframe from the chosen currency
+        if Settings.reload_currency_data is True:
+            # Get from the Binance API all the historical data of the chosen currency
+            self.create_data_frame(Client().get_historical_klines(Settings.currency, Settings.interval,
+                                                                  Settings.start_period, Settings.end_period))
+            self.df.to_csv('data.csv')
+        else:
+            # Use the data.csv to get the data of the chosen currency
+            self.create_data_frame(pd.read_csv('data.csv'))
+
+        # Converting timestamps into dates
+        self.df['timestamp'] = pd.to_datetime(self.df['timestamp'], unit='ms')
+
+    def create_data_frame(self, source):
+        # Create a Data Frame out of our source (klinesT or csv file)
+        self.df = pd.DataFrame(source, columns=['timestamp', 'open', 'high',
+                                                'low', 'close', 'volume',
+                                                'close_time', 'quote_av', 'trades',
+                                                'tb_base_av', 'tb_quote_av', 'ignore'])
+        # Deleting columns that are useless
+        self.to_delete = ['ignore', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av']
+        for i in self.to_delete:
+            del self.df[i]
+
+        # Converting the used columns to numeric
+        self.to_numeric = ['close', 'high', 'low', 'open']
+        for i in self.to_numeric:
+            self.df[i] = pd.to_numeric(self.df[i])
+
+Data = GetData()
+
+
